bc0b23ebcd6b292732c3877c0ae65187fa86d370
RUHUL AMIN <ruhulamin15jun1999@gmail.com>
2025-09-21 10:23:51 +0600

    service panel updated

--- src/Components/Backend/Settings/General/General.js
+++ src/Components/Backend/Settings/General/General.js
@@ -1,5 +1,5 @@
 import { __ } from "@wordpress/i18n";
-import { PanelBody, SelectControl} from "@wordpress/components";
+import { PanelBody, SelectControl } from "@wordpress/components";
 import { themeOptions } from "../../../../utils/options";
 import { themeSwitch } from "../../../../utils/functions";
 import { ItemsPanel } from "../../../../../../bpl-tools/Components";
@@ -10,12 +10,9 @@ const General = ({ attributes, setAttributes }) => {
 
   return (
     <>
-      <PanelBody
-        className="bPlPanelBody"
-        title={__("Themes", "service-card-layout")}
-        initialOpen={true}
-      >
+      <PanelBody className="bPlPanelBody">
         <SelectControl
+          label={"Themes"}
           value={theme}
           options={themeOptions}
           onChange={(val) => setAttributes(themeSwitch(val, attributes))}
@@ -29,22 +26,22 @@ const General = ({ attributes, setAttributes }) => {
       <PanelBody
         className="bPlPanelBody"
         title={__("Services", "service-card-layout")}
-        initialOpen={false}
+        initialOpen={true}
       >
-       <ItemsPanel
-        {...{ attributes, setAttributes,activeServiceIdx }}
-        arrKey="services"
-        activeIndex={activeServiceIdx}
-        newItem={{
-          "icon": "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' id='code'><path d='M5.854 4.854a.5.5 0 1 0-.708-.708l-3.5 3.5a.5.5 0 0 0 0 .708l3.5 3.5a.5.5 0 0 0 .708-.708L2.707 8zm4.292 0a.5.5 0 0 1 .708-.708l3.5 3.5a.5.5 0 0 1 0 .708l-3.5 3.5a.5.5 0 0 1-.708-.708L13.293 8z'/></svg>",
-          "title": "Service Card Layout Designs",
-          "description": "Three completely different layout approaches for service cards, each with unique structure and visual hierarchy"
-        }}
-        ItemSettings={ServicesItemsPanel}
-        design="sortable"
-        // title="name"
-      />
-
+        <ItemsPanel
+          {...{ attributes, setAttributes, activeServiceIdx }}
+          arrKey="services"
+          activeIndex={activeServiceIdx}
+          newItem={{
+            icon: "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' id='code'><path d='M5.854 4.854a.5.5 0 1 0-.708-.708l-3.5 3.5a.5.5 0 0 0 0 .708l3.5 3.5a.5.5 0 0 0 .708-.708L2.707 8zm4.292 0a.5.5 0 0 1 .708-.708l3.5 3.5a.5.5 0 0 1 0 .708l-3.5 3.5a.5.5 0 0 1-.708-.708L13.293 8z'/></svg>",
+            title: "Service Card Layout Designs",
+            description:
+              "Three completely different layout approaches for service cards, each with unique structure and visual hierarchy",
+          }}
+          ItemSettings={ServicesItemsPanel}
+          design="sortable"
+          // title="name"
+        />
       </PanelBody>
     </>
   );
